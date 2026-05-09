096d3097f9f77f527aeff25cfef304e0c9f0f7a4
Anjali <anjali.rana@indiamart.com>
2026-05-09 22:57:33 +0530

    React Component Intro

--- App.js
+++ App.js
@@ -4,8 +4,25 @@ import ReactDOM from "react-dom/client";
 //JSX Is not a part of React, neither HTML not fully javascript
 // JSX is a java script XML to make easy elements in react or anywhere else
 const JSXHeading = <h1>Hello from JSX</h1>;
+//if want to put multiple lines in JSX wrap them around () braces
+// const JSXHeading = (
+//<h1>Hello from JSX</h1>
+//<h2> Welcome</h2>
+// );
 // bable transpiler convert this JSX to React.CreateElement("h1",,"Hello from JSX"), then .render function converts it to real html and render on page
 //JSX should be converst to javascript before running it on browser , browser dont understand JSX
+
+// React Component - 1. Funtional (New Way) 2 .class Component (older way)
+// you cn simply conver tthe above jsx to eact functional component by putting it inside function (most common practice arrow funct)
+// in jsx inside () werything is treat as a single element  and
+//  in arrow if there is only single thing to be return we ignore puting curly brace and retunr ststement
+// like const fn=()={return true } can be write as const fn=()=>true;
+const HeadingComponent = () => <h1>Hello from React Component</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
-root.render(JSXHeading);
+// root.render(JSXHeading);
+
+// to render Functional Componenet inside root wrap them in <> with closeing tag ,
+//  thats how bable understand it is a functional component and transform it to React component which than
+// .render of React converts to real html and render it on dom
+
+root.render(<HeadingComponent />);
