e1c8004dc9ac57f676116a6a8349031143559a68
Anjali <anjali.rana@indiamart.com>
2026-05-15 11:34:19 +0530

    Assignment-5 Search Filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,8 +3,27 @@ import { RestrauntList } from "../utils/RestrauntListData";
 import { useState } from "react";
 
 const Body = () => {
+  const searchFilter = (e) => {
+    let searchtext = e.target.value;
+    if (searchtext === "") {
+      setList(RestrauntList); // reset full list
+    } else {
+      let searchList = resList.filter((restraunt) => {
+        return restraunt.name
+          .toLowerCase()
+          .startsWith(searchtext.toLowerCase()); // must use return if {} in arrow function
+      });
+      setList(searchList);
+    }
+  };
+  const topRateFilter = () => {
+    const newresList = resList.filter(
+      (restraunt) => restraunt.avgRating >= 4.5,
+    );
+    setList(newresList);
+  };
   const [resList, setList] = useState(RestrauntList);
-  console.log(resList);
+  let Timeout;
   return (
     <div className="body">
       <div className="search-container">
@@ -12,14 +31,14 @@ const Body = () => {
           type="text"
           name="Restraunt"
           placeholder="Search your food/Restraunt"
+          onKeyUp={(e) => {
+            searchFilter(e);
+          }}
         />
         <button
           className="filter-btn"
-          onClick={(e) => {
-            const newresList = resList.filter(
-              (restraunt) => restraunt.avgRating >= 4.5,
-            );
-            setList(newresList);
+          onClick={() => {
+            topRateFilter();
           }}>
           Top Rated Restaurants
         </button>
@@ -35,3 +54,23 @@ const Body = () => {
 };
 
 export default Body;
+
+/***** 
+
+ HERE AS WE ARE FILTERING FROM RESLIS ONLY ONCE TOP RATED CLICK RESLIS IS FILTERED , NOW SEARCHING WILL SEARCH ONLY IN ALREADY FILTERED LIST 
+
+
+now resList is already filtered
+React has no way to restore original data
+✔ Correct idea
+
+You must ALWAYS keep:
+
+Original full list (unchanged)
+Filtered list (for UI)
+✔ Solution 1 (BEST PRACTICE)
+const originalList = RestrauntList;
+const [resList, setList] = useState(originalList);
+
+
+*/
